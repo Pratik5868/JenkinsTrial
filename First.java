be2688006b5968efd972e20c806d55336d769d45
pratik <pratikpatil5567@gmail.com>
2024-03-29 11:35:15 +0530

    Initial Commit

---
+++ First.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class First{
+    public static void main(String[] args) {
+        System.out.println("This is java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+    
+}
