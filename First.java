c9dbdba706f99f8e5c91214b2230fe9285fa3e3f
pratik <pratikpatil5567@gmail.com>
2024-03-29 11:56:25 +0530

    second commit

--- First.java
+++ First.java
@@ -5,6 +5,7 @@ public class First{
         System.out.println("This is java program");
         System.out.println("Output from java program");
         System.out.println(new Date());
+        System.out.println("Let's finish the game");
     }
     
 }
